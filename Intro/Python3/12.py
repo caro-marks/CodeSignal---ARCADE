511d9d9545c0ff8c3077b4a71eb9a3e925bb1135
Marcos Nolasco <marcosabn@gmail.com>
2021-10-22 16:21:22 -0300

    solving some more in python

--- Intro/Python3/12.py
+++ Intro/Python3/12.py
@@ -6,13 +6,14 @@
 def allLongestStrings1(inputArray):
     max_size = 0
     for string in inputArray:
-        if len(string)>max_size:
-            max_size=len(string)
+        if len(string) > max_size:
+            max_size = len(string)
     longestStrings = []
     for string in inputArray:
         if len(string) == max_size:
             longestStrings.append(string)
     return longestStrings
+
 
 def allLongestStrings2(inputArray):
     return [i for i in inputArray if len(i) == len(max(inputArray, key=len))]
@@ -20,24 +21,28 @@ def allLongestStrings2(inputArray):
 # COMMON CHARACTER COUNTS
 # # Given two string, find the number of common characters between them.
 
+
 def commonCharacterCount1(s1, s2):
-    s3=s2
+    s3 = s2
     for letter in s1:
         if letter in s3:
-            s3=s3.replace(letter,'',1)
+            s3 = s3.replace(letter, '', 1)
     return len(s2)-len(s3)
 
+
 def commonCharacterCount2(s1, s2):
-    com = [min(s1.count(i),s2.count(i)) for i in set(s1)]
+    com = [min(s1.count(i), s2.count(i)) for i in set(s1)]
     return sum(com)
 
 # is Lucky
 # # Given a ticket number n, determine if the sum of its first half of the digits is equal to the sum of the second half, or not.
 
+
 def isLucky1(n):
     numbers = [int(item) for item in str(n)]
     half = len(numbers)//2
     return sum(numbers[half:]) == sum(numbers[:half])
+
 
 def isLucky2(n):
     s = str(n)
@@ -45,8 +50,9 @@ def isLucky2(n):
     left, right = s[:pivot], s[pivot:]
     return sum(map(int, left)) == sum(map(int, right))
 
-# Sort by Height    
+# Sort by Height
 # # Rearrange the even numbers heights in a non-descending order
+
 
 def sortByHeight1(a):
     items = sorted([item for item in a if item > 0])
@@ -58,19 +64,21 @@ def sortByHeight1(a):
             del(items[0])
     return a
 
+
 def sortByHeight2(a):
 
     l = sorted([i for i in a if i > 0])
-    for n,i in enumerate(a):
+    for n, i in enumerate(a):
         if i == -1:
-            l.insert(n,i)
+            l.insert(n, i)
     return l
 
 # reverse in Parantheses
 # # Write a function that reverses in (possibly nested) parentheses in the input string.
 
+
 def reverseInParentheses1(inputString):
-    stack =['']
+    stack = ['']
     for i in inputString:
         if i == "(":
             stack.append('')
@@ -81,8 +89,10 @@ def reverseInParentheses1(inputString):
             stack[-1] += i
     return stack.pop()
 
+
 def reverseInParentheses2(s):
     return eval('"' + s.replace('(', '"+("').replace(')', '")[::-1]+"') + '"')
+
 
 def reverseInParentheses3(s):
     for i in range(len(s)):
@@ -96,17 +106,20 @@ def reverseInParentheses3(s):
 # alternating sums
 # # Return an array of 2 integers, where its elements are the weight.
 
+
 def alternatingSums1(a):
-    total_weight_1 = sum([w for index, w in enumerate(a) if index%2==0])
+    total_weight_1 = sum([w for index, w in enumerate(a) if index % 2 == 0])
     total_weight_2 = sum(a)-total_weight_1
-    return [total_weight_1,total_weight_2]
+    return [total_weight_1, total_weight_2]
+
 
 def alternatingSums2(a):
 
-    return [sum(a[::2]),sum(a[1::2])]
+    return [sum(a[::2]), sum(a[1::2])]
 
 # Add Border
 # # Given a rectangular matrix of characters, add a border of asterisks * to it.
+
 
 def addBorder1(picture):
     size = len(picture[0])+2
@@ -116,8 +129,72 @@ def addBorder1(picture):
     borded.append(size*'*')
     return borded
 
+
 def addBorder2(picture):
-    l=len(picture[0])+2
-    return ["*"*l]+[x.center(l,"*") for x in picture]+["*"*l]
+    l = len(picture[0])+2
+    return ["*"*l]+[x.center(l, "*") for x in picture]+["*"*l]
+
+# Are Similar
+# # Given two arrays, check if one can be obtained from another by syapping at most one pair of elements
 
 
+def areSimilar(a, b):
+    pairs = tuple(zip(a, b))
+    diffs = []
+    for pair in pairs:
+        if pair[0] != pair[1]:
+            diffs.append(list(pair))
+    if len(diffs) == 1 or len(diffs) > 2:
+        return False
+    elif len(diffs) == 2 and diffs[0] != list(reversed(diffs[1])):
+        return False
+    else:
+        return True
+
+
+def areSimilar(A, B):
+    return sorted(A) == sorted(B) and sum([a != b for a, b in zip(A, B)]) <= 2
+
+# arrayChange
+# # Find the minimal nuimber of moves required to obtain a strictly increasing sequence from an input array
+
+
+def arrayChange(inputArray):
+    loops = 0
+    for index in range(1, len(inputArray)):
+        diff = inputArray[index-1] - inputArray[index] + 1
+        if diff > 0:
+            inputArray[index] += diff
+            loops += diff
+    return loops
+
+
+def arrayChange(inputArray):
+    target = inputArray[0] - 1
+    steps = 0
+    for i in inputArray:
+        if i > target:
+            target = i
+        else:
+            target = target + 1
+            steps += target - i
+
+    return steps
+
+# Palindrome Rearranging
+# # Given a string, find out if tis chars can form a palindrome
+
+
+def palindromeRearranging(inputString):
+    chars = set(list(inputString))  # all single chars
+    return sum([list(inputString).count(char) % 2 for char in chars]) < 2
+
+
+def palindromeRearranging(inputString):
+    s = set()
+    for c in inputString:
+        if c in s:
+            s.remove(c)
+        else:
+            s.add(c)
+    return len(s) <= 1
---
+++ Intro/Python3/13.py
@@ -0,0 +1,228 @@
+# ISLAND OF KNOWLEDGE
+
+# areEquallyStrong
+# # Check if 2 pairs of numbers has the same weigth
+
+def areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight):
+    return sorted((yourLeft, yourRight)) == sorted((friendsLeft, friendsRight))
+
+
+def areEquallyStrong(ul, ur, fl, fr):
+    return max(ul, ur) == max(fl, fr) and min(ul, ur) == min(fl, fr)
+
+
+# arrayMaximalAdjacentDifference
+# #Given an array of integers, find the maximal absolute difference between any two of its adjacent elements.
+
+def arrayMaximalAdjacentDifference(inputArray):
+    max_diff = 0
+    for index in range(1, len(inputArray)-1):
+        diff = max(abs(inputArray[index-1]-inputArray[index]),
+                   abs(inputArray[index+1]-inputArray[index]))
+        if diff > max_diff:
+            max_diff = diff
+    return max_diff
+
+
+def arrayMaximalAdjacentDifference(a):
+    diffs = [abs(a[i]-a[i+1]) for i in range(len(a)-1)]
+    return max(diffs)
+
+# isIPv4Address
+# # Fivern a string, find out if it satisfies the ipv4 address naming rules
+
+
+def isIPv4Address(inputString):
+    splited_string = inputString.split('.')
+    if len(splited_string) != 4:
+        return False
+    else:
+        for i in splited_string:
+            if i.isnumeric():
+                if i == '':
+                    return False
+                elif len(i) > 1 and i[0] == '0':
+                    return False
+                else:
+                    if int(i) > 255 or int(i) < 0:
+                        return False
+            else:
+                return False
+    return True
+
+
+def isIPv4Address(s):
+    p = s.split('.')
+    return len(p) == 4 and all(n.isdigit() and 0 <= int(n) < 256 for n in p)
+
+# avoidObstacles
+# # Find the minimal length of the jump enough to avoid all the obstacles
+
+
+def avoidObstacles(inputArray):
+    initial_position = 0
+    step = 2
+    position = initial_position
+    while position < max(inputArray):
+        position += step
+        if position not in inputArray:
+            continue
+        else:
+            step += 1
+            position = initial_position
+    return step
+
+
+def avoidObstacles(inputArray):
+    c = 2
+    while True:
+        if sorted([i % c for i in inputArray])[0] > 0:
+            return c
+        c += 1
+
+# Box Blur
+# # Return the blurred image as an integer, with the fractions rounded down.
+
+
+def boxBlur(image):
+    filtered_image = []
+    for row_index in range(1, len(image)-1):
+        row = []
+        for col_index in range(1, len(image[0])-1):
+            result = 0
+            result += image[row_index-1][col_index-1]
+            result += image[row_index-1][col_index]
+            result += image[row_index-1][col_index+1]
+            result += image[row_index][col_index-1]
+            result += image[row_index][col_index]
+            result += image[row_index][col_index+1]
+            result += image[row_index+1][col_index-1]
+            result += image[row_index+1][col_index]
+            result += image[row_index+1][col_index+1]
+            row.append(int(result/9))
+        filtered_image.append(row)
+    return filtered_image
+
+
+def boxBlur(image):
+    return [[int(sum(sum(x[i:i+3]) for x in image[j:j+3])/9) for i in range(len(image[j])-2)] for j in range(len(image)-2)]
+
+
+# Minwsweeper
+# # Given a matrix of booleans, return a minesweeper
+
+def minesweeper(matrix):
+    def count_mines_around(matrix, row, col):
+        total_mines = 0
+        total_mines += find_mines(matrix, row-1, col-1)
+        total_mines += find_mines(matrix, row-1, col)
+        total_mines += find_mines(matrix, row-1, col+1)
+        total_mines += find_mines(matrix, row, col-1)
+        total_mines += find_mines(matrix, row, col+1)
+        total_mines += find_mines(matrix, row+1, col-1)
+        total_mines += find_mines(matrix, row+1, col)
+        total_mines += find_mines(matrix, row+1, col+1)
+        return total_mines
+
+    def find_mines(matrix, row, col):
+        if row < 0 or col < 0:
+            return 0
+        elif col > len(matrix[0])-1 or row > len(matrix)-1:
+            return 0
+        elif matrix[row][col] is True:
+            return 1
+        else:
+            return 0
+
+    def main():
+        mines = []
+        for i_row in range(len(matrix)):
+            row = []
+            for i_col in range(len(matrix[0])):
+                row.append(count_mines_around(matrix, i_row, i_col))
+            mines.append(row)
+        return mines
+    main()
+
+
+def minesweeper(matrix):
+    r = []
+    for i in range(len(matrix)):
+        r.append([])
+        for j in range(len(matrix[0])):
+            l = -matrix[i][j]
+            for x in [-1, 0, 1]:
+                for y in [-1, 0, 1]:
+                    if 0 <= i+x < len(matrix) and 0 <= j+y < len(matrix[0]):
+                        l += matrix[i+x][j+y]
+            r[i].append(l)
+    return r
+
+
+# # # RAINS OF REASON # # #
+
+# Array Replace
+# # Given an array of integers, replace all the occurrences of elemToReplace with substitutionElem
+
+def arrayReplace(inputArray, elemToReplace, substitutionElem):
+    response = []
+    for i in inputArray:
+        if i == elemToReplace:
+            response.append(substitutionElem)
+        else:
+            response.append(i)
+    return response
+
+
+def arrayReplace(inputArray, elemToReplace, substitutionElem):
+    return [i if i != elemToReplace else substitutionElem for i in inputArray]
+
+# evenDigitsOnly
+# # Check if all digits of the given integer are even.
+
+
+def evenDigitsOnly(n):
+    for digit in str(n):
+        if int(digit) % 2 != 0:
+            return False
+    return True
+
+
+def evenDigitsOnly(n):
+    return all([int(i) % 2 == 0 for i in str(n)])
+
+# variable name
+# # Check if the string has only letters, digits and underscores, but not starting by digits
+
+
+def variableName(name):
+    if name[0].isdigit():
+        return False
+    else:
+        for char in name:
+            if not char.isdigit() and not char.isalpha() and char != '_':
+                return False
+    return True
+
+
+def variableName(name):
+    return name.isidentifier()
+
+# Alphabetic Shift
+# # Given a string, your task is to replace each of its characters by the next one
+# # in the English alphabet; i.e. replace a with b, replace b with c, etc
+# # (z would be replaced by a).
+
+
+def alphabeticShift(i):
+    i = list(i)
+    for x in range(len(i)):
+        if i[x] == 'z':
+            i[x] = 'a'
+        else:
+            i[x] = chr(ord(i[x])+1)
+    return "".join(i)
+
+
+def alphabeticShift(inputString):
+    return ''.join((chr(ord(i)+1) if i != "z" else "a" for i in inputString))
